444b20fcfd53210923058a36597cdfbec9b45f5d
Joy12122022 <joynq1212@gmail.com>
2024-09-11 23:14:08 +0700

    test

---
+++ scripts.js
@@ -0,0 +1,6 @@
+const serverUrl = "http://joynguyen.online";
+
+document.getElementById('link2048').href = `${serverUrl}/gameoffline/2048`;
+document.getElementById('link3dtetris').href = `${serverUrl}/gameoffline/3dtetris`;
+document.getElementById('linkAlienAttack').href = `${serverUrl}/gameoffline/alienattack`;
+document.getElementById('linkBalanceBall').href = `${serverUrl}/gameoffline/balanceball`;

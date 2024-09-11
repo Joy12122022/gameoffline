36d81ffa200789e1ffe8f5f509996851ce1c47ce
Joy12122022 <joynq1212@gmail.com>
2024-09-12 00:05:06 +0700

    update

--- scripts.js
+++ scripts.js
@@ -1,6 +1,12 @@
-const serverUrl = "http://joynguyen.online";
+let serverUrl = '';
 
-document.getElementById('link2048').href = `${serverUrl}/gameoffline/2048`;
-document.getElementById('link3dtetris').href = `${serverUrl}/gameoffline/3dtetris`;
-document.getElementById('linkAlienAttack').href = `${serverUrl}/gameoffline/alienattack`;
-document.getElementById('linkBalanceBall').href = `${serverUrl}/gameoffline/balanceball`;
+fetch('/CNAME')
+    .then(response => response.text())
+    .then(data => {
+        serverUrl = data.trim();
+        document.getElementById('link2048').href = `${serverUrl}/2048`;
+        document.getElementById('link3dtetris').href = `${serverUrl}/3dtetris`;
+        document.getElementById('linkAlienAttack').href = `${serverUrl}/alienattack`;
+        document.getElementById('linkBalanceBall').href = `${serverUrl}/balanceball`;
+    })
+    .catch(error => console.error('Error fetching CNAME file:', error));
